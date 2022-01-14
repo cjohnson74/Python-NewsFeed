556e2ead37b8d72e1effda7a853d1f8911e14c52
Carson Johnson <carson74johnson@gmail.com>
2022-01-14 16:13:30 -0500

    added / and /login routes

---
+++ app/routes/home.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('home', __name__, url_prefix='/')
+
+@bp.route('/')
+def index():
+    return render_template('homepage.html')
+
+@bp.route('/login')
+def login():
+    return render_template('login.html')
