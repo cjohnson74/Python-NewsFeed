275a0722b3044e570e75472dabe4f09bccbb00be
Carson Johnson <carson74johnson@gmail.com>
2022-01-14 18:36:19 -0500

    created a User class that inherits from the Base class

---
+++ app/models/User.py
@@ -0,0 +1,10 @@
+from tkinter import N
+from app.db import Base
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
