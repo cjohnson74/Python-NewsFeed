f2d5f32cb8365cb7a97dc98269b75c2df47d19e0
Carson Johnson <carson74johnson@gmail.com>
2022-01-14 18:55:18 -0500

    import validates from sqlalchemy.orm into User.py file. In the User class created a validate_email method to the class that @validates('email') decorator wraps.

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,6 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
 
 class User(Base):
     __tablename__ = 'users'
@@ -7,3 +8,10 @@ class User(Base):
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
+
+    @ validates('email')
+    def validate_email(self, key, email):
+        # make sure email address contains @ character
+        assert '@' in email
+
+        return email
