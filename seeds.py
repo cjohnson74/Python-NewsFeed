cda2529b6a6e053acd01ff6f0a2527f86edf79ed
Carson Johnson <carson74johnson@gmail.com>
2022-01-14 20:20:21 -0500

    added seed data for using the Post model

--- seeds.py
+++ seeds.py
@@ -18,4 +18,15 @@ db.add_all([
 
 db.commit()
 
+# insert posts
+db.add_all([
+    Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
+    Post(title='Morbi non quam nec dui luctus rutrum', post_url='https://nasa.gov/donec.json', user_id=1),
+    Post(title='Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue', post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
+    Post(title='Nunc purus', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
+    Post(title='Pellentesque eget nunc', post_url='http://google.ca/nam/nulla/integer.aspx', user_id=4)
+])
+
+db.commit()
+
 db.close()
