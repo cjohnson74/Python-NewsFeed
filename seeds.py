7b9278282b89f9ac49abd5690cb9930b8da893a3
Carson Johnson <carson74johnson@gmail.com>
2022-01-14 20:51:41 -0500

    insert comments into the database in the seeds.py file

--- seeds.py
+++ seeds.py
@@ -29,4 +29,15 @@ db.add_all([
 
 db.commit()
 
+# insert comments
+db.add_all([
+  Comment(comment_text='Nunc rhoncus dui vel sem.', user_id=1, post_id=2),
+  Comment(comment_text='Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', user_id=1, post_id=3),
+  Comment(comment_text='Aliquam erat volutpat. In congue.', user_id=2, post_id=1),
+  Comment(comment_text='Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', user_id=2, post_id=3),
+  Comment(comment_text='In hac habitasse platea dictumst.', user_id=3, post_id=3)
+])
+
+db.commit()
+
 db.close()
