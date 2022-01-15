88396f5ee554ddb39648d25af66cc13bd28ed58f
Carson Johnson <carson74johnson@gmail.com>
2022-01-14 21:00:59 -0500

    fixed reference to users table bug in Comment model

--- app/models/Comment.py
+++ app/models/Comment.py
@@ -7,7 +7,7 @@ class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(255), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     post_id = Column(Integer, ForeignKey('posts.id'))
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
