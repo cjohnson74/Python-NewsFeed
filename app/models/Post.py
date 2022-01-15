101352337d37e14d3c2c8e2b37e7820e98c95c91
Carson Johnson <carson74johnson@gmail.com>
2022-01-14 20:17:01 -0500

    added the user relationship to the Post model

--- app/models/Post.py
+++ app/models/Post.py
@@ -12,3 +12,4 @@ class Post(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    user = relationship('User')
