546d9e8d5922551d3caf3ea852d22dfd680fbe75
Carson Johnson <carson74johnson@gmail.com>
2022-01-15 15:01:21 -0500

    querying for Posts in order of data created in the home.py file

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,14 +1,28 @@
 from flask import Blueprint, render_template
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
+
 @bp.route('/')
 def index():
+    # get all posts
+    db = get_db()
+    posts = (
+        db
+        .query(Post)
+        .order_by(Post.created_at.desc())
+        .all()
+    )
+
     return render_template('homepage.html')
+
 
 @bp.route('/login')
 def login():
     return render_template('login.html')
+
 
 @bp.route('/post/<id>')
 def single(id):
