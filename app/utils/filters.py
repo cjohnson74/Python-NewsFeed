26695f04b45fc5bc2ca52757994e07dedb3691df
Carson Johnson <carson74johnson@gmail.com>
2022-01-17 11:42:15 -0500

    added function that correctly formats/pluralizing words

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -4,6 +4,14 @@ def format_date(date):
 def format_url(url):
     return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
 
+def format_plural(amount, word):
+    if amount != 1:
+        return word + 's'
+
+    return word
+
 # test code
 from datetime import datetime
 print(format_date(datetime.now()))
+print(format_url('http://google.com/test/')) # expected output: google.com
+print(format_url('https://www.google.com?q=test')) # expected output: google.com
