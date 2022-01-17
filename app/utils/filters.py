622e08b8a254f6025e9bdf50319d5bd7314ea64f
Carson Johnson <carson74johnson@gmail.com>
2022-01-17 11:38:42 -0500

    added code that removes all extrenous information from the urls

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -1,2 +1,9 @@
 def format_date(date):
     return date.strftime('%m/%d/%y')
+
+def format_url(url):
+    return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
+
+# test code
+from datetime import datetime
+print(format_date(datetime.now()))
