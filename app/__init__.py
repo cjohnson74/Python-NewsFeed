aa4d3eb4263e4f8449b56e1cb3a92c8389692efb
Carson Johnson <carson74johnson@gmail.com>
2022-01-17 11:44:26 -0500

    imported filters into app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.routes import home, dashboard
 from app.db import init_db
+from  app.utils import filters
 
 def create_app(test_config=None):
     # set up app config
