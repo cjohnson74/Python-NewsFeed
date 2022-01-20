11d8c29ac67ff77f93154006f3cd38c8e730d55d
Carson Johnson <carson74johnson@gmail.com>
2022-01-20 15:03:53 -0500

    properly referenced the data (dictionary) using brackets. Then adding the newUser to the database (db) and then using the commit method to officially update the database

--- app/routes/api.py
+++ app/routes/api.py
@@ -7,10 +7,16 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 @bp.route('/users', methods=['POST'])
 def signup():
     data = request.get_json()
+
+    # create a new user
     newUser = User(
-        username = data.username,
-        email = data.email,
-        password = data.password
+        username = data['username'],
+        email = data['email'],
+        password = data['password']
     )
+
+    # save in database
+    db.add(newUser)
+    db.commit()
 
     return ''
