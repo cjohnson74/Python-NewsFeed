b5b599ba0df5262ffc6ef2d07e607f87b02e9280
Carson Johnson <carson74johnson@gmail.com>
2022-01-20 15:17:09 -0500

    added a try... except block and the newUser attempts to be created and saved the to database and if an error occurs the except block is ran and uses the jsonify method to print out the error in json to the front end

--- app/routes/api.py
+++ app/routes/api.py
@@ -4,20 +4,25 @@ from app.db import get_db
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
+
 @bp.route('/users', methods=['POST'])
 def signup():
     data = request.get_json()
     db = get_db()
 
-    # create a new user
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+    try:
+        # attempt create a new user
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
-    # save in database
-    db.add(newUser)
-    db.commit()
+        # save in database
+        db.add(newUser)
+        db.commit()
+    except:
+        # insert failed, so send error to front end
+        return jsonify(message = 'Signup failed'), 500
 
     return jsonify(id = newUser.id)
