867e9cc039328edadeb0a47049011d93a61f6b2f
Carson Johnson <carson74johnson@gmail.com>
2022-01-20 18:43:30 -0500

    added an upvote route that creates that tries to create a newVote and if not sucessful returns a jsonified error message

--- app/routes/api.py
+++ app/routes/api.py
@@ -85,3 +85,25 @@ def comment():
         return jsonify(message = 'Comment failed'), 500
 
     return jsonify(id = newComment.id)
+
+@bp.route('/posts/upvote', methods=['PUT'])
+def upvote():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # create a new vote with incoming id and session id
+        newVote = Vote(
+            post_id = data['post_id']
+            user_id = session.get('user_id')
+        )
+
+        db.add(newVote)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message = 'Upvote failed'), 500
+
+    return '', 204
