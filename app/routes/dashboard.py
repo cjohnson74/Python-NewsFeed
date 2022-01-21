5c729bffae11055e77397a01ef5ecaa73ddac134
Carson Johnson <carson74johnson@gmail.com>
2022-01-20 22:30:12 -0500

    queried the database the get all the user's posts and order them by date created

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,10 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
 def dash():
+    db = get_db()
+    posts = (
+        db.query(Post)
+        .filter(Post.user_id == session.get('user_id'))
+        .order_by(Post.created_at.desc())
+        .all()
+    )
     return render_template('dashboard.html')
+
 
 @bp.route('/edit/<id>')
 def edit(id):
