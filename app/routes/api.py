d64acb7cfff25fe34025f16462b8cce4fd1c8afc
Carson Johnson <carson74johnson@gmail.com>
2022-01-21 10:51:24 -0500

    added the login_required decorator to all the create(), update(id), delete(id), upvote(), and comment() routes in api.py

--- app/routes/api.py
+++ app/routes/api.py
@@ -3,6 +3,8 @@ from app.models import User, Post, Comment, Vote
 from app.db import get_db
 import sys
 
+from app.utils.auth import login_required
+
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 
@@ -64,6 +66,7 @@ def login():
     return jsonify(id = user.id)
 
 @bp.route('/comments', methods=['POST'])
+@login_required
 def comment():
     data = request.get_json()
     db = get_db()
@@ -87,6 +90,7 @@ def comment():
     return jsonify(id = newComment.id)
 
 @bp.route('/posts/upvote', methods=['PUT'])
+@login_required
 def upvote():
     data = request.get_json()
     db = get_db()
@@ -109,6 +113,7 @@ def upvote():
     return '', 204
 
 @bp.route('/posts', methods=['POST'])
+@login_required
 def create():
     data = request.get_json()
     db = get_db()
@@ -132,6 +137,7 @@ def create():
     return jsonify(id = newPost.id)
 
 @bp.route('/posts/<id>', methods=['PUT'])
+@login_required
 def update(id):
     data = request.get_json()
     db = get_db()
@@ -155,6 +161,7 @@ def update(id):
     return '', 204
 
 @bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
 def delete(id):
     db = get_db()
 
